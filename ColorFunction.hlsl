0f0a6319934774a2d59732b81d83c8f3b7701d9a
ayaha401 <75297336+ayaha401@users.noreply.github.com>
2024-09-12 00:57:14 +0900

    Update ColorFunction.hlsl
    入力された色が1.0を超えるか、0より小さい場合はエラーを知らせるメソッドを作成した

--- ColorFunction.hlsl
+++ ColorFunction.hlsl
@@ -27,4 +27,20 @@ float3 hsv2rgb(float3 c)
     return c.z * lerp(float3(1.0, 1.0, 1.0), rgb, c.y);
 }
 
+// checkColorの要素のうち、1つでも1.0を超えるか、0以下ならエラーの色を出す
+float4 isColorOutOfRange(float4 checkColor, float4 errorOverColor = float4(1.0, 0.0, 1.0, 1.0), float4 errorMinusColor = float4(1.0, 0.0, 1.0, 1.0))
+{
+    if (max(max(max(checkColor.r, checkColor.g), checkColor.b), checkColor.a) > 1.0)
+    {
+        return errorOverColor;
+    }
+
+    if (min(min(min(checkColor.r, checkColor.g), checkColor.b), checkColor.a) < 0.0)
+    {
+        return errorMinusColor;
+    }
+
+    return checkColor;
+}
+
 #endif
